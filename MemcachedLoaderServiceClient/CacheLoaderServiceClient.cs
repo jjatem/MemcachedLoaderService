5391e9ed3d43ca6c23e30bae9e147a2fc20123b6
jjatem <jjatem@hotmail.com>
2016-01-23 22:28:26 -0500

    Saving client solution progress

--- MemcachedLoaderServiceClient/CacheLoaderServiceClient.cs
+++ MemcachedLoaderServiceClient/CacheLoaderServiceClient.cs
@@ -10,6 +10,61 @@ namespace MemcachedLoaderServiceClient
 {
     public class CacheLoaderServiceClient
     {
+        public string MemcachedServer { get; set; }
+        public int MemcachedPort { get; set; }
 
+        public MemcachedClient Client
+        {
+            get
+            {
+                return client;
+            }
+        }
+
+        private ServerConnectionCollection MemCachedServers = new ServerConnectionCollection();
+        private MemcachedClient client;
+
+        public CacheLoaderServiceClient(string server, int port)
+        {
+            this.MemcachedServer = server;
+            this.MemcachedPort = port;
+
+            /*
+             * Add Server from Config Settings
+             */
+            this.MemCachedServers.Add(this.MemcachedServer, port: this.MemcachedPort);
+
+            /*
+             * Create the client
+             */
+            IConnectionProvider provider = new ConnectionProvider(MemCachedServers);
+            this.client = new MemcachedClient(provider);
+        }
+
+        public string GetStoredJSONForKey(string key)
+        {
+            string retval = string.Empty;
+
+            if (this.IsServerConnectionOpen())
+            {
+
+            }
+
+            return retval;
+        }
+
+        public bool IsServerConnectionOpen()
+        {
+            try
+            {
+                return (this.client != null && this.client.Versions().Count() > 0);
+            }
+            catch 
+            {
+                //don't do anything it will return as false
+            }
+
+            return false;
+        }
     }
 }
