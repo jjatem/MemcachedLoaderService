7dcbcdfbb700c426d0b362c2cd7170922e75486e
jjatem <jjatem@hotmail.com>
2016-01-30 21:22:46 -0500

    Adding Support to Memory Caching Windows Service to load the information in a Redis Cache

--- MemcachedLoaderService/ConfigurationObjects/RedisSettings.cs
+++ MemcachedLoaderService/ConfigurationObjects/RedisSettings.cs
@@ -11,5 +11,32 @@ namespace MemcachedLoaderService
         public string Server { get; set; }
         public int Port { get; set; }
         public string Password { get; set; }
+
+
+        /// <summary>
+        /// Builds the appropriate Redis Server connection string
+        /// for the ServiceStack.Redis Client
+        /// </summary>
+        /// <returns></returns>
+        public string GetConnectionString()
+        {
+            string connStr = string.Empty;
+
+            if (!string.IsNullOrWhiteSpace(Server) && Port > 0)
+            {
+                if (string.IsNullOrWhiteSpace(Password))
+                {
+                    connStr = string.Format("redis://{0}:{1}", Server, Port);
+                }
+                else
+                {
+                    connStr = string.Format(@"redis://clientid:{0}@{1}:{2}", Password, Server, Port);
+                }
+            }
+
+            return connStr;
+        }
+
+
     }
 }
